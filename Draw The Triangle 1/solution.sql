63fc9faa89ad72f3d8ff8ce2dcda7b6ed466e20f
ZbigniewKorycki <Zbigniewkorycki.info@gmail.com>
2023-11-23 12:38:52 +0100

    add solution to exercise Draw The Triangle 1

---
+++ Draw The Triangle 1/solution.sql
@@ -0,0 +1,10 @@
+WITH RECURSIVE Numbers AS (
+  SELECT 1 AS number
+  UNION ALL
+  SELECT number + 1
+  FROM Numbers
+  WHERE number < 20 
+)
+SELECT REPEAT('* ', n.number) AS Pattern
+FROM Numbers n
+ORDER BY n.number DESC;
